5aea3e26df414bfced05be26c4b6d21f882f9cbc
owenjoycecoughlan <129763794+owenjoycecoughlan@users.noreply.github.com>
2024-11-27 09:04:21 -0600

    added ui to rockpaperscissors

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 let choice;
-let userScore = 0
-let computerScore = 0
+let userScore = 0;
+let computerScore = 0;
 let num;
-let gameRounds = 0
+let gameRounds = 0;
 
-while (gameRounds < 5) {
- 
+const rockButton = document.createElement("button");
+rockButton.textContent = "rock";
+const paperButton = document.createElement("button");
+paperButton.textContent = "paper";
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent = "scissors";
+
+const div = document.querySelector("div")
+div.appendChild(rockButton);
+div.appendChild(paperButton);
+div.appendChild(scissorsButton);
+
+let user = ""
+rockButton.addEventListener("click", () => {
+    user = "rock";
+    playRound();
+})
+paperButton.addEventListener("click", () => {
+    user = "paper";
+    playRound();
+})
+scissorsButton.addEventListener("click", () => {
+    user = "scissors";
+    playRound();
+})
+
+function playRound() {
     num = Math.random()
     if (num <= 1/3) {
         choice = "rock";
@@ -19,7 +44,6 @@ while (gameRounds < 5) {
     }
     console.log(`The computer chose ${choice}`)
 
-    const user = prompt("Rock, paper, or scissors?")
     if (
         user === "rock" && choice === "scissors"
         || user === "scissors" && choice === "paper"
@@ -48,6 +72,13 @@ while (gameRounds < 5) {
     } else {
         console.log("Next round... \n")
     }
-
-    gameRounds++
 }
+
+
+
+// In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+// For now, remove the logic that plays exactly five rounds.
+// Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+// Add a div for displaying results and change all of your console.logs into DOM methods.
+// Display the running score, and announce a winner of the game once one player reaches 5 points.
+// You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
